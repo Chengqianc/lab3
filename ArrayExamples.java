15389bdbfa24a1784340db2aa046fe07aa1de0a6
Chengqianc <qiancheng0712@gmail.com>
2022-10-14 23:33:19 -0700

    Update ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,6 +4,12 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    /**
+    Bug was here. The following used to be:
+    for(int i = 0; i < arr.length; i += 1 ) {
+      arr[i] = arr[arr.length - i - 1];
+    }
+     */
     for(int i = 0; i < arr.length / 2; i += 1 ) {
       int temp = arr[i];
       arr[i] = arr[arr.length - i - 1];
